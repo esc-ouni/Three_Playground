bfee5ef112976e89f74cffa001474f3b18a1a69c
esc-ouni <idouni@student.1337.ma>
2024-10-04 20:08:36 +0100

    Adding-Model-To-Physic-World/Added

--- Imported-Models-GLTF/src/script.js
+++ Imported-Models-GLTF/src/script.js
@@ -24,7 +24,7 @@ floor.material.side = THREE.DoubleSide;
 scene.add(floor)
 
 // Lights
-const ambientLight = new THREE.AmbientLight(0xffffff, 3.14)
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.14)
 scene.add(ambientLight)
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
@@ -81,28 +81,69 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 //GLTF Loading
 const GLTFLoaderr = new GLTFLoader();
 
-//Load ping pong Table
-let i = 0
-GLTFLoaderr.load('/models/PP_Table/scene.gltf', function (gltf){
+
+//createTrimesh
+function createTrimesh(geometry) {
+    if (!geometry.attributes.position) {
+        return null; // No position data
+    }
+
+    const vertices = geometry.attributes.position.array;
+    const indices = geometry.index ? geometry.index.array : null;
+
+    if (!indices) {
+        // If the geometry is not indexed, create indices
+        const numVertices = vertices.length / 3;
+        const generatedIndices = new Uint16Array(numVertices);
+        for (let i = 0; i < numVertices; i++) {
+            generatedIndices[i] = i;
+        }
+        return new cannon.Trimesh(Array.from(vertices), Array.from(generatedIndices));
+    }
+
+    return new cannon.Trimesh(Array.from(vertices), Array.from(indices));
+}
+//
+
+// Other Options
+// GLTFLoaderr.load('/models/PP_Table/scene.gltf', function (gltf){
 // GLTFLoaderr.load('/models/table_v2/scene.gltf', function (gltf){
+// model.scale.set(2, 2, 2)
+// model.position.y -= 0.04;
+        
+//Load ping pong Table
+let shape = null;
+let mass = 0;
+
+GLTFLoaderr.load('/models/chinese_tea_table_4k.gltf/chinese_tea_table_4k.gltf', function (gltf){
     const model = gltf.scene;
     model.scale.set(4, 4, 4)
-    // model.scale.set(2, 2, 2)
-    model.position.y -= 0.01;
+    model.position.y += 0.001;
     model.traverse(function (node) {
-          
         if (node.isMesh) {
             node.castShadow = true;
             node.receiveShadow = true;
-            node.material.wireframe = true;
-            console.log('hello', i++)
+            // node.material.wireframe = true;
+
+            shape = createTrimesh(node.geometry);
+            if (shape != null) {
+                let body = new cannon.Body({
+                    mass: mass,
+                    shape: shape,
+                });
+    
+                body.position.copy(node.position);
+                body.quaternion.copy(node.quaternion);
+                console.log('Body Created !', body);
+                PhysicWorld.addBody(body);
+            }
+
         }
     })
-  
     scene.add(model);
 })
-
 //
+
 const hit_sound = new Audio("/sounds/ping_pong.mp3");
 
 const Pong_Ball_colide = (Collision) => {
@@ -166,7 +207,6 @@ PhysicWorld.gravity.set(0, - 8.92, 0);
 
 const concreteMaterial = new cannon.Material('concrete');
 const plasticMaterial  = new cannon.Material('plastic');
-const metalMaterial  = new cannon.Material('metal');
 
 const ContactMaterial = new cannon.ContactMaterial(
     concreteMaterial,
@@ -214,9 +254,9 @@ PhysicWorld.addBody(planeBody);
 //To Add it To Dat Gui It has to be inside of an Object
 const BallCreator = {}
 BallCreator.createBall = () => {
-    let x = (Math.random() - 0.5) * 10
+    let x = (Math.random() - 0.5) * 3
     let y = 12
-    let z = (Math.random() - 0.5) * 10
+    let z = (Math.random() - 0.5) * 3
     createSphere(new THREE.Vector3(x, y, z))
 }
 
@@ -242,8 +282,6 @@ gui.add(BallCreator, 'createBall')
 gui.add(BallCreator, 'reset')
 //
 //
-
-
 
 
 //  Animate
